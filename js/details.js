597fa6a05c59574c4a53259a6e45b30c7c6f1b4f
AhmedYasser35 <ahmedyasserabds@gmail.com>
2026-03-25 21:10:43 +0200

    responsive update

--- js/details.js
+++ js/details.js
@@ -1,15 +1,17 @@
-var data
-var image
-var title
-var description
-var platform
-var category
-var url
-var game
-var cartona = ''
+var data;
+var image;
+var title;
+var description;
+var platform;
+var category;
+var url;
+var cartona = "";
+
 export async function detailsGames(id) {
-  
-  if (!id) return; // no game selected yet
+  if (!id) return;
+
+  const spinner = document.getElementById("spinner");
+  spinner.classList.remove("d-none");
 
   const options = {
     headers: {
@@ -20,49 +22,56 @@ export async function detailsGames(id) {
 
   var response = await fetch(
     `https://free-to-play-games-database.p.rapidapi.com/api/game?id=${id}`,
-    options
+    options,
   );
-    data = await response.json();
-    console.log(data.thumbnail);
-    image = data.thumbnail
-    title = data.title
-    description = data.description;
-    platform = data.platform;
-    category = data.genre;
-    url = data.game_url;
-    console.log(url);
-const spinner = document.getElementById("spinner");
-spinner.classList.add("d-none");
-    
+
+  data = await response.json();
+  image = data.thumbnail;
+  title = data.title;
+  description = data.description;
+  platform = data.platform;
+  category = data.genre;
+  url = data.game_url;
+
+  spinner.classList.add("d-none");
 }
-    
-export async function detailsDisplay(id){
-    await detailsGames(id)
-    cartona = `<div class="container">
-    <div class="details-title text-white d-flex justify-content-between">
-    <h3>Details Game</h3>
-    <button class="btn "><i class="fa-solid fa-x"></i></button>
-    </div>
-    <div class="game-details">
-    <div class="row">
-    <div class="col-xl-4">
-    <img src='${image}' alt="">
-    </div>
-    <div class="col-xl-8 text-white">
-    <h4>Title: ${title}</h4>
-    <h6>Category: <span class='bg-info p-1 border-0 rounded-2 text-black fw-bold'>${category}</span></h6>
-    <h6>Platform: <span class='bg-info p-1 border-0 rounded-2 text-black fw-bold'>${platform}</span></h6>
-    <h6>Status: <span class='bg-info p-1 border-0 rounded-2 text-black fw-bold'>Live</span></h6>
-    <p>${description}</p>
-    <a href="${url}" target='_blank'><button type="button" class="btn btn-outline-warning text-white fw-bold">show game</button></a>
-    </div>
-    </div>
-    </div>
+
+export async function detailsDisplay(id) {
+  await detailsGames(id);
+
+  cartona = `
+    <div class="container py-4">
+      <div class="details-title text-white d-flex justify-content-between align-items-center mb-4">
+        <h3 class="m-0">Details Game</h3>
+        <button class="btn btn-close btn-close-white" id="btnClose"></button>
+      </div>
+      <div class="game-details">
+        <div class="row g-4">
+          <div class="col-md-12 col-lg-4">
+            <img src='${image}' class="w-100 rounded-3 shadow" alt="${title}">
+          </div>
+          <div class="col-md-12 col-lg-8 text-white">
+            <h4 class="mb-3">Title: ${title}</h4>
+            <div class="info-badges mb-3 d-flex flex-wrap gap-2">
+               <h6 class="m-0">Category: <span class='bg-info p-1 px-2 border-0 rounded-2 text-black fw-bold'>${category}</span></h6>
+               <h6 class="m-0">Platform: <span class='bg-info p-1 px-2 border-0 rounded-2 text-black fw-bold'>${platform}</span></h6>
+               <h6 class="m-0">Status: <span class='bg-info p-1 px-2 border-0 rounded-2 text-black fw-bold'>Live</span></h6>
+            </div>
+            <p class="small lh-lg">${description}</p>
+            <a href="${url}" target='_blank' class="btn btn-outline-warning text-white fw-bold mt-3 px-4">Show Game</a>
+          </div>
+        </div>
+      </div>
     </div>`;
-    document.getElementById("details").innerHTML = cartona;
-    document.querySelector('.btn').addEventListener('click',(event_1)=>{
-      document.getElementById('details').classList.add('d-none')
-      document.querySelector(".main-game").classList.remove("d-none");
-      
-    })
-  }
+
+  const detailsContainer = document.getElementById("details");
+  detailsContainer.innerHTML = cartona;
+  detailsContainer.classList.remove("d-none");
+  document.querySelector(".main-game").classList.add("d-none");
+
+  document.getElementById("btnClose").addEventListener("click", () => {
+    detailsContainer.classList.add("d-none");
+    document.querySelector(".main-game").classList.remove("d-none");
+    window.scrollTo(0, 0);
+  });
+}
--- js/ui.js
+++ js/ui.js
@@ -1,4 +1,4 @@
-export var gameId = null; // the clicked game ID
+export var gameId = null;
 
 export class Game {
   constructor(title, desc, thumbnail, genre, platform, id) {
@@ -24,10 +24,9 @@ export async function loadGames(category) {
 
   const response = await fetch(
     `https://free-to-play-games-database.p.rapidapi.com/api/games?category=${category}`,
-    options
+    options,
   );
   var data = await response.json();
-  console.log(data)
   spinner.classList.add("d-none");
 
   return data.map(
@@ -38,8 +37,8 @@ export async function loadGames(category) {
         item.thumbnail,
         item.genre,
         item.platform,
-        item.id
-      )
+        item.id,
+      ),
   );
 }
 
@@ -49,22 +48,24 @@ export async function displayGame(category) {
 
   for (let game of games) {
     cartona += `
-      <div class="game-card col-md-3 p-0 rounded-2 d-flex flex-column justify-content-between" role='button' data-id='${game.id}'>
-        <div class="body p-3">
-          <div class="game-img">
-            <img src="${game.thumbnail}" alt="" class="w-100 border-0 mb-4 rounded-2">
-          </div>
-          <div class="game-content">
-            <div class='game-title d-flex justify-content-between'>
-              <h3 class="fs-5 text-white">${game.title}</h3>
-              <p class='text-white bg-primary py-1 fw-bolder px-2 border-0 rounded-3' id='free'>Free</p>
+      <div class="col-sm-6 col-md-4 col-lg-3 p-3">
+        <div class="game-card h-100 rounded-2 d-flex flex-column justify-content-between" role='button' data-id='${game.id}'>
+          <div class="body p-3">
+            <div class="game-img">
+              <img src="${game.thumbnail}" alt="${game.title}" class="w-100 border-0 mb-4 rounded-2">
             </div>
-            <p class="text-center">${game.desc}</p>
+            <div class="game-content">
+              <div class='game-title d-flex justify-content-between align-items-center gap-2'>
+                <h3 class="fs-6 text-white mb-0">${game.title}</h3>
+                <span class='badge bg-primary' id='free'>Free</span>
+              </div>
+              <p class="text-center mt-2 small">${game.desc}</p>
+            </div>
           </div>
-        </div>
-        <div class="card-footer d-flex justify-content-between border-bottom-0 border-end-0 border-start-0 px-3 py-2">
-          <p class="m-0 text-white border-0 rounded-3 fw-bolder">${game.genre}</p>
-          <p class="m-0 text-white border-0 rounded-3 fw-bolder">${game.platform}</p>
+          <div class="card-footer d-flex justify-content-between align-items-center border-top px-3 py-2">
+            <p class="m-0 text-white rounded-3 fw-bolder">${game.genre}</p>
+            <p class="m-0 text-white rounded-3 fw-bolder">${game.platform}</p>
+          </div>
         </div>
       </div>
     `;
